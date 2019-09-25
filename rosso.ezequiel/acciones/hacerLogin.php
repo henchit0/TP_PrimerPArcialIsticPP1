d66ccd82f5f2f167e6b23f0b6ee0a397dc23e389
Ezequiel Rosso <45835786+henchit0@users.noreply.github.com>
2019-09-25 20:37:27 -0300

    Cambios en el login
    Estoy cambiando las validaciones dentro del hacerLogin

---
+++ rosso.ezequiel/acciones/hacerLogin.php
@@ -0,0 +1,42 @@
+<?php
+	
+	$checkUsuario = $_GET['inputUsuario'];
+	$checkPassword = $_GET['inputPassword'];
+	$booUsuario = 0;
+	$booPassword = 0;
+
+	if (empty($checkUsuario) || empty($checkPassword)) 
+	{
+		header("Location: ../login.php?error=camposvacios");
+		exit();
+	}
+	else
+	{
+		$archivo = fopen("usuarios.txt", "r") or die("Imposible arbrir el archivo");
+	
+		while(!feof($archivo)) 
+		{
+			$objeto = json_decode(fgets($archivo));
+			if ($objeto->usuario == $checkUsuario) 
+			{	
+				$booUsuario = 1;
+				if ($objeto->password == $checkPassword)
+				{
+					header("Location: ../login.php?exito=signup");
+					fclose($archivo);
+					exit();
+				}			
+			}
+		 	
+		}
+		if ($booUsuario == 0) {
+			header("Location: ../login.php?error=usuarioincorrecto");
+			fclose($archivo);
+			exit();
+		}
+
+		fclose($archivo);
+	}	
+	header("Location: ../login.php");
+	exit();
+?>
